9f2a9dae0262777d4a0fd5c4be714225a8942f72
Alexander Stefurishin <a.stefurishin@gmail.com>
2019-10-24 20:31:23 +0300

    support command-line parameters; add run script

--- BenchApp/Program.cs
+++ BenchApp/Program.cs
@@ -1,41 +1,63 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using FluentArgs;
+using FluentArgs.Validation;
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace BenchApp
 {
     static class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            var totalRows = 5_000_000;
-            var value = "ASD123";
-            var tblCount = 10;
-            var batchSizeBase = 5_000;
-            var batchSizeIncrement = 5_000;
+            FluentArgsBuilder
+                .NewWithDefaultConfigs()
+                .WithApplicationDescription("Benchmark of sqlite3 bulk inserts with different pragmas.")
+                .Parameter<int>("-i", "--batchSizeIncrement")
+                    .WithDescription("Growth of 'batchSizeBase' parameter in each test set run.")
+                    .WithValidation(i => i >= 1)
+                    .IsOptionalWithDefault(5_000)
+                .Parameter<int>("-b", "--batchSizeBase")
+                    .WithDescription("Minimal number of inserts in a transaction, will be incremented by 'batchSizeIncrement' in each test set run.")
+                    .WithValidation(b => b >= 1)
+                    .IsOptionalWithDefault(5_000)
+                .Parameter<int>("-t", "--tableCount")
+                    .WithDescription("Number of tables to use for inserts.")
+                    .WithValidation(t => t >= 1)
+                    .IsOptionalWithDefault(10)
+                .Parameter<string>("-s", "--string")
+                    .WithDescription("Value to insert (along with sequential integer primary key).")
+                    .IsOptionalWithDefault("string")
+                .Parameter<int>("-r", "--rowCount")
+                    .WithDescription("Total number of rows to insert in each test.")
+                    .WithValidation(r => r >= 100)
+                    .IsOptionalWithDefault(5_000_000)
+                .Call(totalRows => value => tblCount => batchSizeBase => batchSizeIncrement =>
+                   {
+                       Console.WriteLine("batchSize;default;sync-off;jm-wal;jm-off;lm-excl;sync-off&jm-off&lm-excl");
+                       for (int i = 0; i < 40; i++)
+                       {
+                           var batchSize = batchSizeBase + batchSizeIncrement * i;
+                           var commits = Math.Max(1, totalRows / batchSize);
 
-            Console.WriteLine("batchSize;default;sync-off;jm-wal;jm-off;lm-excl;sync-off&jm-off&lm-excl");
-
-            for (int i = 0; i < 40; i++)
-            {
-                var batchSize = batchSizeBase + batchSizeIncrement * i;
-                var commits = totalRows / batchSize;
-
-                Console.WriteLine(new StringBuilder().AppendJoin(";",
-                    batchSize,
-                    RunTestSet("default", i, Array.Empty<string>(), tblCount, commits, batchSize, value),
-                    RunTestSet("sync-off", i, new[] { "synchronous = OFF" }, tblCount, commits, batchSize, value),
-                    RunTestSet("jm-wal", i, new[] { "journal_mode = WAL" }, tblCount, commits, batchSize, value),
-                    RunTestSet("jm-off", i, new[] { "journal_mode = OFF" }, tblCount, commits, batchSize, value),
-                    RunTestSet("lm-excl", i, new[] { "locking_mode = EXCLUSIVE" }, tblCount, commits, batchSize, value),
-                    RunTestSet("sync-off&jm-off&lm-excl", i, new[] {
-                        "synchronous = OFF",
-                        "journal_mode = OFF",
-                        "locking_mode = EXCLUSIVE" }, tblCount, commits, batchSize, value)
-                    ));
-            }
+                           Console.WriteLine(new StringBuilder().AppendJoin(";",
+                               batchSize,
+                               RunTestSet("default", i, Array.Empty<string>(), tblCount, commits, batchSize, value),
+                               RunTestSet("sync-off", i, new[] { "synchronous = OFF" }, tblCount, commits, batchSize, value),
+                               RunTestSet("jm-wal", i, new[] { "journal_mode = WAL" }, tblCount, commits, batchSize, value),
+                               RunTestSet("jm-off", i, new[] { "journal_mode = OFF" }, tblCount, commits, batchSize, value),
+                               RunTestSet("lm-excl", i, new[] { "locking_mode = EXCLUSIVE" }, tblCount, commits, batchSize, value),
+                               RunTestSet("sync-off&jm-off&lm-excl", i, new[] {
+                                "synchronous = OFF",
+                                "journal_mode = OFF",
+                                "locking_mode = EXCLUSIVE" }, tblCount, commits, batchSize, value)
+                               ));
+                       }
+                   })
+                .Parse(args);
         }
 
         private static double RunTestSet(
@@ -47,7 +69,11 @@ namespace BenchApp
             int batchSize,
             object value)
         {
-            using var connection = CreateConnection($"{shortName}_{runIndex}.sqlite", tblCount, pragmas);
+            using var connection = CreateConnection(
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                $"{shortName}_{runIndex}.sqlite"),
+                tblCount,
+                pragmas);
             long lastRev = 0L;
             var commands = PrepareCommands(tblCount, connection, value);
             var seqResult = RunTest(commits, () =>
